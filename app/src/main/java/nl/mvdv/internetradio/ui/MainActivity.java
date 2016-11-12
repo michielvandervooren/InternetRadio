417f8008c3ae4d6c8e8e429560148b4cf0d71d36
Michiel van der Vooren <michiel.vander.vooren@gmail.com>
2016-11-12 15:25:55 +0100

    removed Fragment

--- app/src/main/java/nl/mvdv/internetradio/ui/MainActivity.java
+++ app/src/main/java/nl/mvdv/internetradio/ui/MainActivity.java
@@ -1,8 +1,8 @@
 package nl.mvdv.internetradio.ui;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +10,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import nl.mvdv.internetradio.Constants;
@@ -25,6 +27,11 @@ public class MainActivity extends AppCompatActivity implements GetTrackInfoHandl
 
     private TextView nowPlayingView;
     private TextView nextTrackView;
+    private ImageButton playButton;
+    private ImageButton pauseButton;
+    private Button linkButton;
+    private String nowPlayingTxt;
+    private String nextTxt;
 
     private Handler handler;
     private GetTrackInfoHandler getTrackInfoHandler;
@@ -32,9 +39,12 @@ public class MainActivity extends AppCompatActivity implements GetTrackInfoHandl
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
 
-        Button playButton = (Button) findViewById(R.id.play_button);
+        playButton = (ImageButton) findViewById(R.id.play_btn);
+        pauseButton = (ImageButton) findViewById(R.id.pause_btn);
+        linkButton = (Button) findViewById(R.id.goto_website);
         nowPlayingView = (TextView) findViewById(R.id.nowTextView);
         nextTrackView = (TextView) findViewById(R.id.nextTextView);
 
@@ -45,18 +55,41 @@ public class MainActivity extends AppCompatActivity implements GetTrackInfoHandl
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Button button = (Button) v;
                 Intent service = new Intent(MainActivity.this, MediaPlayerService.class);
                 if (!MediaPlayerService.isRunning()) {
                     service.setAction(Constants.Actions.STARTFOREGROUND_ACTION);
                     MediaPlayerService.setRunning(true);
-                    button.setText(R.string.pause_txt);
+                    updateUI();
                 } else {
                     service.setAction(Constants.Actions.STOPFOREGROUND_ACTION);
                     MediaPlayerService.setRunning(false);
-                    button.setText(R.string.play_txt);
+                    updateUI();
                 }
                 startService(service);
+            }
+        });
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent service = new Intent(MainActivity.this, MediaPlayerService.class);
+                if (!MediaPlayerService.isRunning()) {
+                    service.setAction(Constants.Actions.STARTFOREGROUND_ACTION);
+                    MediaPlayerService.setRunning(true);
+                    updateUI();
+                } else {
+                    service.setAction(Constants.Actions.STOPFOREGROUND_ACTION);
+                    MediaPlayerService.setRunning(false);
+                    updateUI();
+                }
+                startService(service);
+            }
+        });
+        linkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openWebsiteIntent = new Intent(Intent.ACTION_VIEW);
+                openWebsiteIntent.setData(Uri.parse(Constants.Urls.WEB_URL));
+                startActivity(openWebsiteIntent);
             }
         });
     }
@@ -71,6 +104,7 @@ public class MainActivity extends AppCompatActivity implements GetTrackInfoHandl
     protected void onResume() {
         super.onResume();
         scheduleGetTrackInfo();
+        updateUI();
     }
 
     @Override
@@ -79,31 +113,27 @@ public class MainActivity extends AppCompatActivity implements GetTrackInfoHandl
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        // The menu xml contains a single action: action_more_info
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_more_info) {
-            // display the more info dialog
-            showMoreInfo();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     public void onTrackInfoReceived(TrackInfo info) {
         if (info == null) {
             Util.toast(this, "Geen track informatie beschikbaar");
         } else {
-            nowPlayingView.setText("Je luistert naar: " + info.getNowPlaying());
-            nextTrackView.setText("Hierna: " + info.getNext());
+            nowPlayingTxt = info.getNowPlaying();
+            nextTxt = info.getNext();
+        }
+        updateUI();
+    }
+
+    private void updateUI() {
+        if (MediaPlayerService.isRunning()) {
+            playButton.setEnabled(false);
+            pauseButton.setEnabled(true);
+            nowPlayingView.setText(nowPlayingTxt);
+            nextTrackView.setText(nextTxt);
+        } else {
+            playButton.setEnabled(true);
+            pauseButton.setEnabled(false);
+            nowPlayingView.setText("");
+            nextTrackView.setText("");
         }
     }
 
@@ -115,8 +145,4 @@ public class MainActivity extends AppCompatActivity implements GetTrackInfoHandl
         handler.removeCallbacks(getTrackInfoHandler);
     }
 
-    private void showMoreInfo() {
-        MoreInfoFragment moreInfoFragment = new MoreInfoFragment();
-        moreInfoFragment.show(getSupportFragmentManager(), null);
-    }
 }
